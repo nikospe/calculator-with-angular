42688c1cef8dda2fdaa64b82cb332578a00a53a9
niksope <nikosper.gm@gmail.com>
2017-06-03 23:32:43 +0300

    style update

--- keypad.js
+++ keypad.js
@@ -9,21 +9,24 @@ app.component('keypad', {
         onClick: '<'
     },
     template: `<div id="numbers">
-        <button 
-            class = "nums" 
-            ng-repeat = "number in [0,1,2,3,4,5,6,7,8,9]" 
-            ng-click = "$ctrl.buttonClick(number)">
-            {{number}}
-        </buttons></div><div id="actions">
-        <button
-            class = "symbols"
-            ng-repeat = 'symbol in ["+","-","*","/","="]'
-            ng-click = "$ctrl.buttonClick(symbol)">
-            {{symbol}}
-        </button>
-        <button
-            id = "clear"
-            ng-click = '$ctrl.buttonClick("clear")'>
-        ce</button></div>`,
+            <button 
+                class = "keys" 
+                ng-repeat = "number in [1,2,3,4,5,6,7,8,9,0]" 
+                ng-click = "$ctrl.buttonClick(number)">
+                {{number}}
+            </buttons>
+        </div>
+        <div id="actions">
+            <button
+                class = "keys symbols"
+                ng-repeat = 'symbol in ["+","-","*","/","="]'
+                ng-click = "$ctrl.buttonClick(symbol)">
+                {{symbol}}
+            </button>
+            <button
+                id = "clear" class="keys"
+                ng-click = '$ctrl.buttonClick("clear")'>
+            ce</button>
+        </div>`,
      controller: KeypadCtrl
 });
